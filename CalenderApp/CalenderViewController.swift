341cc438710553e316f26b80f6a678edbad1d36c
nujudalharbi <Njoud.Aali.med.s@tuwaiq.edu.sa>
2022-08-14 23:10:16 +0300

    Initial Commit

---
+++ CalenderApp/CalenderViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CalenderApp
+//
+//  Created by نجود  on 16/01/1444 AH.
+//
+
+import UIKit
+import CalendarKit
+class CalenderViewController: DayViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       title = "Calender"
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
