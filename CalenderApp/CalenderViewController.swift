2979cdfaeac95e5f40908a7d3f7e4e8a99fda7c4
nujudalharbi <Njoud.Aali.med.s@tuwaiq.edu.sa>
2022-08-16 18:48:28 +0300

    add event in calendar

--- CalenderApp/CalenderViewController.swift
+++ CalenderApp/CalenderViewController.swift
@@ -7,17 +7,74 @@
 
 import UIKit
 import CalendarKit
-class CalenderViewController: DayViewController {
+import EventKit
 
+
+class CalenderViewController: DayViewController {
+    
+   private let eventStore = EKEventStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        title = "Calender"
-        
-        
+        requestAccessToCalender()
+        subscribeToNatifications()
         
         // Do any additional setup after loading the view.
     }
+   
+    func requestAccessToCalender(){
+        
+        eventStore.requestAccess(to: .event) { success , error in
+            
+        }
+        
+    }
+    
+    
+    func subscribeToNatifications(){
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(storeChange(_ :)), name: .EKEventStoreChanged, object: nil)
+        
+    }
+    
+    @objc func storeChange(_ notification : Notification){
+        
+        reloadData()
+        
+    }
+    
+    
+    override func eventsForDate(_ date: Date) -> [EventDescriptor] {
+        let startDate = date
+        var oneDayComponents = DateComponents()
+        oneDayComponents.day = 1
+        let endDate = calendar.date(byAdding: oneDayComponents, to: startDate )!
+        
+        
+        let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+        
+        let eventKitEvents = eventStore.events(matching: predicate)
+        
+        
+        let calenderKitEvents = eventKitEvents.map {
+            ekEvent   -> Event in
+           let ckEvent = Event()
+            let ckEvent1 = ekEvent
+            ckEvent1.startDate = ekEvent.startDate
+            ckEvent1.endDate = ekEvent.endDate
+            ckEvent.isAllDay = ekEvent.isAllDay
+            ckEvent.text = ekEvent.title
+            if let eventColor = ekEvent.calendar.cgColor {
+                
+                ckEvent.color = UIColor(cgColor: eventColor)
+            }
+            
+            return ckEvent
+            
+        }
 
-
+        return calenderKitEvents
+    }
 }
 
